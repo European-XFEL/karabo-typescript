6771fee9607817b57536a7812c022c572392bb0d
Alessandro Silenzi <alessandro.silenzi@gmail.com>
2024-10-10 18:30:39 +0200

    added more tests on `.` syntax

--- tests/karabo-ts-tests/serializer.ts
+++ tests/karabo-ts-tests/serializer.ts
@@ -16,29 +16,40 @@ describe("binary", function() {
         const hsh = parser.read();
         //process.stdout.write(JSON.stringify(hsh));
         //process.stdout.write(JSON.stringify(hsh.value["floatPropertyReadOnly"]));
-        expect(hsh.value_["deviceId"].value.value_).to.equal("Bob");
-        expect(hsh.value_["classId"].value.value_).to.equal("PropertyTest");
-        expect(hsh.value_["alarmCondition"].value.value_).to.equal("none");
-        expect(hsh.value_["boolPropertyReadOnly"].value.value_).to.equal(false);
-        expect(hsh.value_["uint16PropertyReadOnly"].value.value_).to.equal(32000);
-        expect(hsh.value_["int16PropertyReadOnly"].value.value_).to.equal(3200);
-        expect(hsh.value_["uint32PropertyReadOnly"].value.value_).to.equal(32000000);
-        expect(hsh.value_["floatPropertyReadOnly"].value.value_).to.equal(3.1415960788726807);
-        expect(hsh.value_["uint8PropertyReadOnly"].value.value_).to.equal(177);
-        expect(hsh.getValue("deviceId")).to.equal("Bob");
-        expect(hsh.getValue("classId")).to.equal("PropertyTest");
-        expect(hsh.getValue("alarmCondition")).to.equal("none");
-        expect(hsh.getValue("boolPropertyReadOnly")).to.equal(false);
-        expect(hsh.getValue("uint16PropertyReadOnly")).to.equal(32000);
-        expect(hsh.getValue("int16PropertyReadOnly")).to.equal(3200);
-        expect(hsh.getValue("uint32PropertyReadOnly")).to.equal(32000000);
-        expect(hsh.getValue("floatPropertyReadOnly")).to.equal(3.1415960788726807);
-        expect(hsh.getValue("uint8PropertyReadOnly")).to.equal(177);
+        expect(hsh.value_["deviceId"].value.value_).to.be.equal("Bob");
+        expect(hsh.value_["classId"].value.value_).to.be.equal("PropertyTest");
+        expect(hsh.value_["alarmCondition"].value.value_).to.be.equal("none");
+        expect(hsh.value_["boolPropertyReadOnly"].value.value_).to.be.equal(false);
+        expect(hsh.value_["uint16PropertyReadOnly"].value.value_).to.be.equal(32000);
+        expect(hsh.value_["int16PropertyReadOnly"].value.value_).to.be.equal(3200);
+        expect(hsh.value_["uint32PropertyReadOnly"].value.value_).to.be.equal(32000000);
+        expect(hsh.value_["floatPropertyReadOnly"].value.value_).to.be.equal(3.1415960788726807);
+        expect(hsh.value_["uint8PropertyReadOnly"].value.value_).to.be.equal(177);
+        // test getValue
+        expect(hsh.getValue("deviceId")).to.be.equal("Bob");
+        expect(hsh.getValue("classId")).to.be.equal("PropertyTest");
+        expect(hsh.getValue("alarmCondition")).to.be.equal("none");
+        expect(hsh.getValue("boolPropertyReadOnly")).to.be.equal(false);
+        expect(hsh.getValue("uint16PropertyReadOnly")).to.be.equal(32000);
+        expect(hsh.getValue("int16PropertyReadOnly")).to.be.deep.equal(3200);
+        expect(hsh.getValue("uint32PropertyReadOnly")).to.be.deep.equal(32000000);
+        expect(hsh.getValue("floatPropertyReadOnly")).to.be.deep.equal(3.1415960788726807);
+        expect(hsh.getValue("uint8PropertyReadOnly")).to.be.deep.equal(177);
+        // test attributes
         expect(hsh.getAttributeValue("uint8PropertyReadOnly", "sec")).to.be.equal(1631725047n);
         expect(hsh.getAttributeValue("uint8PropertyReadOnly", "frac")).to.be.equal(661721908000000000n);
         expect(hsh.getAttributeValue("uint8PropertyReadOnly", "tid")).to.be.equal(0n);
         expect(hsh.getAttributeValue("uint8PropertyReadOnly", "alarmCondition")).to.be.equal("none");
+
+        expect(hsh.getValue("vectors.boolProperty")).to.be.deep.equal([true,false,true,false,true,false]);
+        expect(hsh.getValue("vectors.uint8Property")).to.be.deep.equal([41, 42, 43, 44, 45, 46]);
+        expect(hsh.getValue("vectors.int16Property")).to.be.deep.equal([20041, 20042, 20043, 20044, 20045, 20046]);
+        expect(hsh.getValue("vectors.uint16Property")).to.be.deep.equal([10041, 10042, 10043, 10044, 10045, 10046]);
+        expect(hsh.getValue("vectors.uint32Property")).to.be.deep.equal([90000041, 90000042, 90000043, 90000044, 90000045, 90000046]);
+        expect(hsh.getValue("vectors.int64Property")).to.be.deep.equal([20000000041n, 20000000042n, 20000000043n, 20000000044n, 20000000045n, 20000000046n]);
+        expect(hsh.getValue("vectors.stringProperty")).to.be.deep.equal(['1111111', '2222222', '3333333', '4444444', '5555555', '6666666']);
         expect(hsh).to.be.instanceOf(Hash);
+        // a spot testing of iterall
         for ( const [key, value, attrs] of hsh.iterall()) {
             if (key == "uint32PropertyReadOnly") {
                 expect(value).not.to.be.instanceOf(Object);
